960c81849540878fe8dfd2c308afaf6b8ac13730
hejp <378540660@qq.com>
2025-03-27 11:05:36 +0800

    fix: 更新代码

---
+++ bigscreen/v2.0.0/static/js/1506.f9feaf98e3.chunk.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunklarge_screen_configuration=self.webpackChunklarge_screen_configuration||[]).push([[1506],{61506:function(e,n,c){c.r(n)}}]);
---
+++ bigscreen/v2.0.0/static/js/2857.16be2de7e1.chunk.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunklarge_screen_configuration=self.webpackChunklarge_screen_configuration||[]).push([[2857],{62857:function(e,n,c){c.r(n)}}]);
